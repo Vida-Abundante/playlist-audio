3136162a58ed7845e464f2f3d4ffc01ec7b703a8
Vida-Abundante <caro_1518@hotmail.com>
2025-12-05 20:24:31 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,13 @@
 self.addEventListener("install", () => {
-  console.log("Vida Abundante instalada 🌸");
+  console.log("Vida Abundante AUDIOS instalada 🌸");
 });
+
+self.addEventListener("activate", () => {
+  console.log("Vida Abundante AUDIOS instalada 🌸");
+});
+
+self.addEventListener("fetch", event => {
+  // Handler mínimo — requerido para PWA instalable
+});
+
+
